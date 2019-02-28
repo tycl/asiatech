0ac0d5620e9a2893f4c14bb364dda13fbb0636a0
tycl <tycl80@gmail.com>
2019-02-28 23:43:07 +0800

    Add the database

--- asiavod/devicesapp/models.py
+++ asiavod/devicesapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class DeviceList(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    devicemac = models.CharField(max_length=20, blank=True, default='')
+    deviceon = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ('devicemac',) 
